9a8c57b1755bcc9d46e9231786f25c05be9f828c
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2023-12-30 18:32:10 +0900

    [update] Storybook with auto import

--- src/.storybook/main.ts
+++ src/.storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
+import AutoImport from "unplugin-auto-import/vite"
+import AutoImportComponents from "unplugin-vue-components/vite"
 
 const config: StorybookConfig = {
   stories: [
@@ -15,6 +17,27 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    if (config.plugins !== undefined) {
+      config.plugins.push(
+        AutoImport({ imports: ["vue"], dts: ".storybook/auto-imports.d.ts" })
+      );
+      config.plugins.push(
+        AutoImportComponents({
+          dirs: ["components"],
+          directoryAsNamespace: true,
+          dts: ".storybook/components.d.ts",
+        })
+      );
+    }
+    return {
+      ...config,
+      define: {
+        ...config.define,
+        global: "window",
+      },
+    };
   },
 };
 export default config;
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,4 +3,5 @@
   <h1 class="text-3xl font-bold underline text-red-600">
     Hello world!
   </h1>
+  <DicesD4 :rolling="false" :n="3" />
 </template>
