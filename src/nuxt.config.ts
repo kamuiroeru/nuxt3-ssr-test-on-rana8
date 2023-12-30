90732965c8ca377517854be19e031de465be059e
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2023-12-30 18:06:48 +0900

    [update] Install Plain tailwindcss

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/storybook", "@nuxt/ui"]
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Title</h1>
+  <h1 class="text-3xl font-bold underline text-red-600">
+    Hello world!
+  </h1>
+</template>
