2a9f4313d12e50db3f89608457e97a891c632ce8
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2023-12-30 21:02:55 +0900

    [update] Enable Rolling

--- src/components/dices/d4/index.vue
+++ src/components/dices/d4/index.vue
@@ -24,12 +24,28 @@ watch(
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">D4</div>
       <div
-        class="rounded shadow-lg w-20 h-20 bg-red-600 flex justify-center items-center"
+        class="rounded shadow-lg w-20 h-20 flex justify-center items-center"
+        :class="rolling ? 'bg-red-400 rotate-animation' : 'bg-red-600'"
       >
-        <p class="text-white text-2xl">
+        <p class="text-2xl" :class="rolling ? 'text-black' : 'text-white'">
           {{ rolling ? randomDice.randomN : n }}
         </p>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.rotate-animation {
+  animation: 0.5s linear infinite rotation
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
