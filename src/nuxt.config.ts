7d20d763855fbf51c609a7ea3a2f147a0d085342
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2024-01-02 01:52:12 +0900

    [update] Install nuxtjs tailwindcss.

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
       enabled: true
     }
   },
-  css: ['~/assets/css/main.css'],
+  css: ['@/assets/css/tailwind.css'],
   app: {
     head: {
       meta: [
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ['@nuxtjs/storybook'],
+  modules: ['@nuxtjs/storybook', '@nuxtjs/tailwindcss'],
   storybook: {
     url: 'http://localhost:6006',
     storybookRoute: '/__storybook__',
